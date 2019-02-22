90ae0031948fd2577acf18e1adb0e59be8a7a0b7
Adam <zsoriadam10@gmail.com>
2019-02-22 11:05:01 +0100

    vmi

--- mastermind/mastermind/Jatek.cs
+++ mastermind/mastermind/Jatek.cs
@@ -8,5 +8,20 @@ namespace mastermind
 {
     partial class Jatek
     {
+        private List<string> probak = new List<string>();
+        private string feladvany;
+        /// <summary>
+        /// a játék előkészítését végző konstruktor
+        /// </summary>
+        /// <param name="f">a kitalálandó feladvány</param>
+        public Jatek(string f)
+        {
+            feladvany = f;
+        }
+        public string Proba(string tipp)
+        {
+            
+            return "";
+        }
     }
 }
