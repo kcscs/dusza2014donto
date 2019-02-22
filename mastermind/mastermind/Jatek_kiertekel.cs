e474ec4eafad73c858a705710f3165d620fa2e2b
kcscs <kcscs10@gmail.com>
2019-02-22 11:14:52 +0100

    keveres

--- mastermind/mastermind/Jatek_kiertekel.cs
+++ mastermind/mastermind/Jatek_kiertekel.cs
@@ -8,5 +8,15 @@ namespace mastermind
 {
     partial class Jatek
     {
+
+        /// <summary>
+        /// Megadja a feladványban többször is előforduló színeket
+        /// </summary>
+        /// <returns></returns>
+        public List<char> TobbszorSzereplok() {
+            List<char> tsz = new List<char>();
+
+            return tsz;
+        }
     }
 }
